4336ee80ea691361bde4ca62cbd9b03badee5f15
Shubhamshinde477 <sshindemeshram@gmail.com>
2022-09-14 18:22:19 +0530

    Adding normalize user

--- app/core/models.py
+++ app/core/models.py
@@ -12,8 +12,8 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password=password)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
         user.save(using=self._db)
 
         return user
